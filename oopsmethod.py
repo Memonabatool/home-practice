2a72c1a141056f686e5a7e7f2de0d75f20f8cea2
Memonabatool <memonabatool089@gmail.com>
2025-04-12 15:48:23 +0500

    second code

--- oopsmethod.py
+++ oopsmethod.py
@@ -16,19 +16,35 @@
 
 
 # constructed method---------------
-class Student:
+#class Student:
     #pass
-        def __init__ (self,name,age,subject,education,marks,grade):
-         self.name = name
-         self.age = age
-         self.subject = subject
-         self.education = education
-         self.marks = marks
-         self.grade = grade
-        def  getStu(self):
-             print(f"{self.name}:{self.age}:{self.subject}:{self.education}:{self.marks}:{self.grade}")
-ab = Student("haider",23,"english",9,70,"B Grade")
-ab.getStu()
+        #def __init__ (self,name,age,subject,education,marks,grade):
+         #self.name = name
+         #self.age = age
+         #self.subject = subject
+         #self.education = education
+         #self.marks = marks
+         #self.grade = grade
+        #def  getStu(self):
+             #print(f"{self.name}:{self.age}:{self.subject}:{self.education}:{self.marks}:{self.grade}")
+#ab = Student("haider",23,"english",9,70,"B Grade")
+#ab.getStu()
 
-
-
+# polymarphizam----------
+class Car:
+    
+        def __init__ (self,name,model):
+            self.name = name
+            self.model = model
+        def fun(self):
+            print(f"{self.name}:{self.model}")
+class Bus:
+        def __init__ (self,name):
+            self.name = name
+        def fun(self):
+            print(f"{self.name}")
+ab= Car("kia",2022)
+ac=Bus("daewo")
+ab.fun()
+ac.fun()
+        
