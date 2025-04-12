3e13ce8bd35d7e400416bdb0f0bd42841a51b497
Memonabatool <memonabatool089@gmail.com>
2025-04-12 15:32:23 +0500

    first commit

--- oopsmethod.py
+++ oopsmethod.py
@@ -1,4 +1,4 @@
-# oops metod in python:
+# oops metod in python-------------
 #class Student:
     #pass
     #def setStuDetails(self,name,age,subject,education,marks,grade):
@@ -13,3 +13,22 @@
 #ab=Student()
 #ab.setStuDetails("mona",23,"science","matric",80,"A grade")
 #ab.getStuDetais() 
+
+
+# constructed method---------------
+class Student:
+    #pass
+        def __init__ (self,name,age,subject,education,marks,grade):
+         self.name = name
+         self.age = age
+         self.subject = subject
+         self.education = education
+         self.marks = marks
+         self.grade = grade
+        def  getStu(self):
+             print(f"{self.name}:{self.age}:{self.subject}:{self.education}:{self.marks}:{self.grade}")
+ab = Student("haider",23,"english",9,70,"B Grade")
+ab.getStu()
+
+
+
